1508eda72bbc2b6117d9b8b99430af320086ff9d
Adrian Rangel <adrian.rangel@gmail.com>
2015-02-01 17:35:39 -0600

    dynamic messages

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 function toggleLike(){
-  console.log(this.textContent);
-  if (this.textContent == 'Like') {
-    this.textContent = 'Unlike';
-  } else {
-    this.textContent = 'Like';
+  this.textContent == 'Like' ? this.textContent = 'Unlike' : this.textContent = 'Like';
+}
+
+function newPost(){
+  textA = document.getElementById('mind');
+  if (textA.textLength < 1){
+    console.log('Mensaje invalido');
+    return false;
   }
+  createPost(textA.value);
+  textA.value = '';
+}
+
+function createPost(message){
+  content = '<div class="message">' +
+    '<div class="message-header">' +
+      '<img class="message-me" src="message-me.jpg" alt="" />' +
+      '<a href="#" style="vertical-align: top;" class="message-name">Adrián Rangel</a>' +
+    '</div>' +
+    '<div class="message-content">' +
+      message +
+    '</div>' +
+    '<div class="message-links">' +
+      '<a href="#" title="Like this" class="like">Like</a> · <a href="#" title="Leave a comment">Comment</a> · <a href="#" title="Send this to friends or post it on your timeline.">Share</a>' +
+    '</div>' +
+  '</div>';
+  messageContainer = document.getElementById('messages-container');
+  messageContainer.innerHTML = content + messageContainer.innerHTML;
+
 }
 
 function handlers(){
@@ -12,6 +35,7 @@ function handlers(){
   for (var i = 0; i < like_links.length; i++) {
     like_links[i].addEventListener('click', toggleLike, false);
   }
+  document.getElementById('post').addEventListener('click', newPost, false);
 }
 
 (function() {
